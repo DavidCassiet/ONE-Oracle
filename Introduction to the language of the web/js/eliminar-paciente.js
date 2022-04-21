7bdef68542f0e5c7b251a51d180f8284aafc0416
DavidCassiet <davidleonelcassiet@hotmail.com>
2022-04-21 19:34:55 -0300

    ...

--- Introduction to the language of the web/js/eliminar-paciente.js
+++
@@ -1,9 +0,0 @@
-let tabla = document.querySelector("#tabla-pacientes");
-
-tabla.addEventListener("dblclick", function (event) {
-  event.target.parentNode.classList.add("fadeOut");
-  setTimeout(function () {
-    event.target.parentNode.remove();
-  }, 500);
-});
-
